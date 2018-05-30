edc849dda135fa3cc95e089705f564101d448ac5
Dennis Zierahn <dezi@kappa-mm.de>
2018-05-30 13:56:49 +0200

    Initial interface specs.

---
+++ libluzirec/src/main/java/Recognize.java
@@ -0,0 +1,41 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Recognize
+{
+    JSONArray users = new JSONArray();
+
+    public void addUserProfile(JSONObject profile)
+    {
+        users.put(profile);
+    }
+
+    public static JSONArray recognizeUser(byte[] pcm_s16le_mono)
+    {
+        JSONArray results = new JSONArray();
+
+        try
+        {
+            JSONObject result1 = new JSONObject();
+
+            result1.put("userid", "lukas");
+            result1.put("score", "0.98");
+
+            results.put(result1);
+
+            JSONObject result2 = new JSONObject();
+
+            result2.put("userid", "dezi");
+            result2.put("score", "0.22");
+
+            results.put(result2);
+
+        }
+        catch (Exception ignore)
+        {
+        }
+
+        return results;
+    }
+
+}
---
+++ libluzirec/src/main/java/Trainer.java
@@ -0,0 +1,45 @@
+import org.json.JSONObject;
+
+public class Trainer
+{
+
+    static
+    {
+        try
+        {
+            JSONObject xxx = new JSONObject("{ userid: \"lukas\" }");
+            byte[] samples = new byte[ 8000 * 2 ];
+
+            trainUser("lukas", xxx, samples);
+        }
+        catch (Exception ignore)
+        {
+        }
+    }
+
+    public static JSONObject loadUserFromDisk(String userid)
+    {
+        return null;
+    }
+
+    public static boolean saveUserToDisk(String userid, JSONObject profile)
+    {
+        return false;
+    }
+
+    public static boolean trainUser(String userid, JSONObject profile, byte[] pcm_s16le_mono)
+    {
+        int len = pcm_s16le_mono.length;
+
+        /*
+            char *pcm_s16le_mono;  // item len = 1 byte
+            unsigned short *pcm_samples; // item len = 2 byte
+
+            pcm_samples = (unsigned short *) pcm_s16le_mono;
+
+            short sample = pcm_samples[ 0 ];
+            short sample = pcm_s16le_mono[ 0 ] << 8 + pcm_s16le_mono[ 1 ];
+        */
+        return false;
+    }
+}
